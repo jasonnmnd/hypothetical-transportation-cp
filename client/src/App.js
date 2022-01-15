ce8c6eb6f4c93e4727e2e7d0e2428e5fbe1e34df
Jason Dong <85291489+jasonnmnd@users.noreply.github.com>
2022-01-15 17:46:12 -0500

    Work on front page login

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Link } from 'react-router-dom';
+import React from 'react';
+import { useState } from 'react';
+import LoginForm from './components/LoginForm';
 
 function App() {
+
+  //Login details, move to database for security
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123"
+  }
+
+  //const [user, setUser] = useState({name: "", email: ""});
+  const [user, setUser] = useState({email: ""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    //console.log(details);
+
+    //TODO: Change to implement backend with database
+    if (details.email == adminUser.email && details.password == adminUser.password) {
+      console.log("Logged in");
+      setUser({
+        //name: details.name,
+        email: details.email
+      });
+    } else {
+      //console.log("Details do not match");
+      setError("Details do not match!");
+    }
+  }
+
+  const Logout = () => {
+    console.log("Logout");
+    setUser({email: ""});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email != "") ? (
+        <div className='welcome'>
+          <h2>Welcome, <span>{"NAME HERE"}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>  
+      ) : (
+      <LoginForm Login={Login} error={error}></LoginForm>
+      )}
     </div>
   );
 }
