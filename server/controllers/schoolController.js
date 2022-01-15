d320146cfdcfc37dd4e1b5db5883e878b62ec256
George Hong <grh17@duke.edu>
2022-01-15 01:13:03 -0500

    Add simple working endpoints for School example

--- server/controllers/schoolController.js
+++ server/controllers/schoolController.js
@@ -1,9 +1,18 @@
 const School = require("../models/school");
 
 exports.school_list = function (req, res) {
-  res.send("NOT IMPLEMENTED: School list");
+  School.find().then((result) => {
+    console.log(result);
+  });
+  // res.send();
+  res.send("NOT (FULLY) IMPLEMENTED: School list");
 };
 
 exports.school_create = function (req, res) {
-  res.send("NOT IMPLEMENTED: School create");
+  const newSchool = new School({
+    name: "Stevens High School",
+    address: "Somewhere in SD",
+  });
+  newSchool.save();
+  res.send("NOT (FULLY) IMPLEMENTED: School create");
 };
