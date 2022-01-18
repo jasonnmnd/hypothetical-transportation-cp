546620bac4b12d2897475aa4682e9c18271b56f8
Lucy Gu <wg74@duke.edu>
2022-01-17 20:14:45 -0500

    admin portal main

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Route, Link } from "react-router-dom";
 import React from "react";
 import { useState, useEffect } from "react";
 import LoginForm from "./components/LoginForm";
+import Header from "./components/header/header";
 
 function App() {
   //Login details, move to database for security
@@ -134,6 +135,28 @@ function App() {
     setUser(emptyUser);
   };
 
+  const WelcomePage = () => {
+    //Somehow redirect back to welcome page (ie, slash nothing)
+  };
+
+  const UserList = () => {
+    //Somehow redirect to user list page (ie, slash users)
+  };
+
+  const SchoolList = () => {
+    //Somehow redirect to school list page (ie, slash schools)
+  };
+  const RouteList = () => {
+    //Somehow redirect to route list page (ie, slash routes)
+  };
+  const StudentList = () => {
+    //Somehow redirect to student list page (ie, slash students)
+  };
+
+  const Account = () => {
+    //Somehow redirect to account page
+  };
+
   // make admin and user pages have their own respective components to simplify this mess here?
   // the table currently show all student information, looks like it should just show the student name
   // and when clicked on name navigate to a new page to show student information
@@ -147,14 +170,55 @@ function App() {
         ></LoginForm>
       ):
       user.admin===true?(
-        <div className="welcome">
-          <h2>
-            Welcome, <span>{user.name}</span>
-          </h2>
-          <button onClick={Logout}>Logout</button>
+        <div className="page">
+          <Header></Header>
+          <div className="sidebar">
+            <ul>
+              <li><button className="currentPage" onClick={WelcomePage}>Main</button></li>
+              <br></br>
+              <li><button onClick={UserList}>Users</button></li>
+              <br></br>
+              <li><button onClick={StudentList}>Students</button></li>
+              <br></br>
+              <li><button onClick={SchoolList}>Schools</button></li>
+              <br></br>
+              <li><button onClick={RouteList}>Routes</button></li>
+            </ul>
+          </div>
+          <div className="main-content">
+            <div className="welcome">
+              <h2>
+                Welcome, <span>{user.name}</span>
+              </h2>
+              <p>
+                This should act as a short navigation overview, the "flavour text"
+              </p>
+              <p>
+                Click the "Users" button in the Menu to see a list of all users on site
+              </p>
+              <p>
+                Click the "Students" button in the Menu to see a list of all students on site
+              </p>
+
+              <p>
+                Click the "Schools" button in the Menu to see a list of all schools on site
+              </p>
+
+              <p>
+                Click the "Routes" button in the Menu to see a list of all bus routes on site
+              </p>
+
+              <p>
+              (currently none works, expecting each button to redirect to another page and render different components)
+              </p>
+              <br></br>
+              <button onClick={Logout}>Logout</button>
+            </div>
+          </div>
         </div>
       ):(
         <div className="page">
+          <Header></Header>
           <div className="welcome">
             <h2>
               Welcome, <span>{user.name}</span>
---
+++ client/src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from './11.png';
+
+function Header(){
+
+    return(
+        <div className="header">
+            <img src={Logo} className="logo"></img>
+        </div>
+    )
+}
+export default Header;
