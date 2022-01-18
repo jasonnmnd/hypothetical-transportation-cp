3675ff45787c5f581da4b30252f9cf162ff90ad3
jasonnmnd <85291489+jasonnmnd@users.noreply.github.com>
2022-01-18 12:26:13 -0500

    added separate component folders and skeleton scripts for admin and parent pages

---
+++ client/src/components/adminPage/adminPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//This page will be used for the admin page to declutter App.js
+function adminPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default adminPage
---
+++ client/src/components/parentPage/parentPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+//This page will be used for the parent page to declutter App.js
+
+function parentPage() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default parentPage
