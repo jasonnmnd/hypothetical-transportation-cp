902868fea5c87d83d54ddb20ad235b6a45e48ca9
George Hong <grh17@duke.edu>
2022-01-19 19:14:26 -0500

    Add example /user/fields/ route
    Currently using built-in representation function.  String still needs to
    be parsed to be useful.

--- hypothetical_transportation/backend/api.py
+++ hypothetical_transportation/backend/api.py
@@ -1,5 +1,9 @@
+import json
+
 from .models import User, Student, School, Route
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .serializers import UserSerializer, StudentSerializer, RouteSerializer, SchoolSerializer
 
 
@@ -9,6 +13,19 @@ class UserViewSet(viewsets.ModelViewSet):
         permissions.AllowAny
     ]
     serializer_class = UserSerializer
+
+    @action(detail=False)
+    def fields(self, request):
+        # TODO: Find better representation format
+        repr_str = repr(UserSerializer()).replace(' ', '')
+        content = repr_str.split('\n')
+        return Response(json.dumps(content))
+
+    # @action(detail=True, methods=['get'])
+    # def students(self, request, pk=None):
+    #     assoc_students = StudentViewSet.queryset.filter(guardian__pk=pk)
+    #     serial_student_data = StudentSerializer(assoc_students, many=True).data
+    #     return Response(serial_student_data)
 
 
 class RouteViewSet(viewsets.ModelViewSet):
