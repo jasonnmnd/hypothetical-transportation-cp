1d764d9d58fdb13a85782db7f9e24ebb638595d4
jasonnmnd <85291489+jasonnmnd@users.noreply.github.com>
2022-01-21 12:59:50 -0500

    added in template tables with mock data for routes, schools, students, users

--- hypothetical_transportation/frontend/src/components/adminPage/pages/AdminRoutesPage.js
+++ hypothetical_transportation/frontend/src/components/adminPage/pages/AdminRoutesPage.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import SidebarSliding from '../components/sidebar/SidebarSliding';
 import Header from '../../header/Header';
+import AdminTable from '../components/table/AdminTable';
 
 function AdminRoutesPage() {
+
+  const title = "Routes"
+  const header = ["route", "school", "num_students"]
+  const data = [
+    {
+      route: 1,
+      school: "Random Elementary School",
+      num_students: 20
+    },
+
+    {
+      route: 2,
+      school: "Random Middle School",
+      num_students: 2
+    },
+
+    {
+      route: 3,
+      school: "Random High School",
+      num_students: 4
+    },
+
+    {
+      route: 4,
+      school: "Random University",
+      num_students: 30
+    },
+
+    {
+      route: 5,
+      school: "Another Random High School",
+      num_students: 300
+    }
+  ]
+
   return (
     <div className='admin-page'>
         <SidebarSliding/>
         <Header textToDisplay={"Admin Portal"}></Header>
+        <AdminTable title={title} header={header} data={data}></AdminTable>
     </div>
   )
 }
--- hypothetical_transportation/frontend/src/components/adminPage/pages/AdminSchoolsPage.js
+++ hypothetical_transportation/frontend/src/components/adminPage/pages/AdminSchoolsPage.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import SidebarSliding from '../components/sidebar/SidebarSliding';
 import Header from '../../header/Header';
+import AdminTable from '../components/table/AdminTable';
 
 function AdminSchoolsPage() {
+
+  //Mock Users Data (API Call later for real data)
+  const title = "Schools"
+  const header = ["school"]
+  const data = [
+    {
+      school: "Random Elementary School"
+    },
+
+    {
+      school: "Random Middle School",
+    },
+
+    {
+      school: "Random High School",
+    },
+
+    {
+      school: "Random University",
+    },
+
+    {
+      school: "Another Random High School",
+    }
+  ]
+
   return (
     <div className='admin-page'>
         <SidebarSliding/>
         <Header textToDisplay={"Admin Portal"}></Header>
+        <AdminTable title={title} header={header} data={data} />
     </div>
   )
 }
