6df032c9a553db325e6aa63b66bc0cc52cd98a78
Lucy Gu <wg74@duke.edu>
2022-01-21 14:08:19 -0500

    admin row remove div to align table

--- hypothetical_transportation/frontend/src/components/adminPage/components/table/adminRow.js
+++ hypothetical_transportation/frontend/src/components/adminPage/components/table/adminRow.js
@@ -3,16 +3,14 @@ import "../../adminPage.css";
 
 const AdminRow = ( {header, data, handleViewClick}) => {
     return (
-        <div className='admin-row'>
-            <tr>
-                {header.map((h,i)=>{
-                    return <td key={i}>{data[h]}</td>
-                })}
-                <td>
-                    <button onClick={() => handleViewClick(data.id)}>View</button>
-                </td>
-            </tr>
-        </div>
+        <tr>
+            {header.map((h,i)=>{
+                return <td key={i}>{data[h]}</td>
+            })}
+            <td>
+                <button onClick={() => handleViewClick(data.id)}>View</button>
+            </td>
+        </tr>
     );
 };
 
