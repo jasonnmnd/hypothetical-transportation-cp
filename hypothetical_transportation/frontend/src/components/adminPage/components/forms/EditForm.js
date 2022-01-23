8a815f3d654da5f2bc146448d78d06c940840773
Lucy Gu <wg74@duke.edu>
2022-01-23 18:15:27 -0500

    edit form working with put request

--- hypothetical_transportation/frontend/src/components/adminPage/components/forms/EditForm.js
+++ hypothetical_transportation/frontend/src/components/adminPage/components/forms/EditForm.js
@@ -1,23 +1,35 @@
 import React, { useState } from "react";
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+
 //not sure if it's going to work
 //right now, for some reason when clicking on save, it redirects to /?id=input&name=input
 //input1: title of form
 //input2: list of fields?
 //input3: a typed object matching the fields
 function EditForm({title, fields, obj, setobj}) {
+    const navigate = useNavigate();
+    const column = title.slice(5,)
     const submit = (e) => {
         e.preventDefault();
         console.log(obj);
         //route to a post to save the data
+        axios
+            .put(`/api/${column}/${obj.id}/`,obj)
+            .then(res =>{
+              console.log(obj)
+              navigate(`/admin/${column}/${obj.id}/`)
+    
+            }).catch(err => console.log(err));
     }
-
+    
     return (
         <div>
             <form>
                 <div className="form-inner">
                     <h2>{title}</h2>
                     {
-                        fields.map((field,i)=>{
+                        fields.filter(f=>f!=="id").map((field,i)=>{
                             return typeof(obj[field])==="boolean" ? 
                             (<div className="form-group" key={i}>
                                 <label htmlFor={field}>{field}</label>
