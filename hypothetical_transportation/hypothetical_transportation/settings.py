5ea798f2d9d749c4eae97c81b6616bbb05668e19
George Hong <grh17@duke.edu>
2022-01-24 13:00:55 -0500

    Add pagination to settings
    use ?offset=X&limit=y

--- hypothetical_transportation/hypothetical_transportation/settings.py
+++ hypothetical_transportation/hypothetical_transportation/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10,
     'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',)
 }
 
