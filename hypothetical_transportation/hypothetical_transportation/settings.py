e5dc3daa50ac938dea01acf279b5de4535238a7d
VCM admin account <vcm@vcm-24706.vm.duke.edu>
2022-01-23 20:07:39 -0500

    adding vcm to list of hosts

--- hypothetical_transportation/hypothetical_transportation/settings.py
+++ hypothetical_transportation/hypothetical_transportation/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = 'django-insecure-dz)xt+ggvmpya26p(yn$y-0gcq1&$tnrj+i_n5*1u0_ek+j3lg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0']
+ALLOWED_HOSTS = [
+        '0.0.0.0',
+        'hypothetical-transportation.colab.duke.edu',
+]
 
 # Application definition
 
