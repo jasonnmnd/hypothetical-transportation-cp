64d47f3801890c9ce738f919a37e4d7cbde566f1
George Hong <grh17@duke.edu>
2022-01-25 12:43:13 -0500

    Fix tests

--- hypothetical_transportation/backend/tests.py
+++ hypothetical_transportation/backend/tests.py
@@ -13,9 +13,11 @@ class AuthenticationObjectConsistency(TestCase):
 
         school = School.objects.create(address='1111 Some St.', name='Eggbert Elementary')
         high_school = School.objects.create(address='7777 Some St.', name='Gravity Falls High School')
-        stan = get_user_model().objects.create_user('stanpines@mysteryshack.com', 'mysteryshack')
+        stan = get_user_model().objects.create_user(email='stanpines@mysteryshack.com', password='mysteryshack',
+                                                    full_name='Stanley Pines', address='618 Gopher Road')
 
-        dan = get_user_model().objects.create(email='manlydan@gmail.com', password='wordpass')
+        dan = get_user_model().objects.create_user(email='manlydan@gmail.com', password='wordpass',
+                                                   full_name='Manly Dan', address='')
 
         mabel = Student.objects.create(first_name='Mabel', last_name='Pines', address='618 Gopher Road', active=True,
                                        school=school, routes=route, guardian=stan)
@@ -37,7 +39,10 @@ class AuthenticationObjectConsistency(TestCase):
         self.client.post('/api/auth/register',
                          json.dumps(
                              {'email': 'stanfordpines@mysteryshack.com',
-                              'password': 'mysteryshack'}),
+                              'full_name': 'Stanford Pines',
+                              'password': 'mysteryshack',
+                              'address': 'Mostly an alternative dimension',
+                              }),
                          content_type='application/json')
         num_users = len(get_user_model().objects.all())
         self.assertEqual(num_users, self.INIT_NUM_USERS + 1)
