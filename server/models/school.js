afdcb77f50ac6e35ca5db070977b3963433ae5ce
George Hong <grh17@duke.edu>
2022-01-27 00:16:40 -0500

    Remove unique name requirement

--- server/models/school.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const SchoolSchema = new Schema({
-  name: String,
-  address: String,
-});
-
-module.exports = mongoose.model("SchoolModel", SchoolSchema);
