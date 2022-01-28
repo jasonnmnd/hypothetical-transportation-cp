f79753cf97003270a4616385222d3b8509c0afe6
Lucy Gu <wg74@duke.edu>
2022-01-28 17:14:50 -0500

    fix edit form prop.obj.address

--- hypothetical_transportation/frontend/src/components/adminPage/components/forms/EditForm.js
+++ hypothetical_transportation/frontend/src/components/adminPage/components/forms/EditForm.js
@@ -67,7 +67,7 @@ function EditForm(props) {
     
     return (
         <div>
-            <div className='confirm_location'>{openModal && <AssistedLocationModal closeModal={setOpenModal} handleConfirmAddress={handleConfirmAddress} address={obj.address}></AssistedLocationModal>}</div>
+            <div className='confirm_location'>{openModal && <AssistedLocationModal closeModal={setOpenModal} handleConfirmAddress={handleConfirmAddress} address={props.obj.address}></AssistedLocationModal>}</div>
             <form>
                 <div className="form-inner">
                     <h2>{props.action+" "+props.column}</h2>
