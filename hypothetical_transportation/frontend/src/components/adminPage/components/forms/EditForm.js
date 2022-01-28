1584c985bf1de8fac426454022c463c9db7b9f5c
Milan Bhat <milan@Milans-MacBook-Air.local>
2022-01-27 20:18:10 -0500

    Tried to reduxify edit form, might not work

--- hypothetical_transportation/frontend/src/components/adminPage/components/forms/EditForm.js
+++ hypothetical_transportation/frontend/src/components/adminPage/components/forms/EditForm.js
@@ -1,40 +1,42 @@
 import React, { useState } from "react";
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 //not sure if it's going to work
 //right now, for some reason when clicking on save, it redirects to /?id=input&name=input
 //input1: title of form
 //input2: list of fields?
 //input3: a typed object matching the fields
-function EditForm({column, fields, obj, setobj, action}) {
+function EditForm(props) {
     const navigate = useNavigate();
-    const col = column.includes("_") ?column.split("_")[1]:column
+    const col = props.column.includes("_") ?props.column.split("_")[1]:props.column
     const submit = (e) => {
-        // console.log(column);
-        // console.log(column.includes("admin"))
-        if(column.includes("admin")){
-            obj.is_staff=true;
+        // console.log(props.column);
+        // console.log(props.column.includes("admin"))
+        if(props.column.includes("admin")){
+            props.obj.is_staff=true;
         }
-        if(column.includes("route")){
-            obj.school=column.split("_")[0];
+        if(props.column.includes("route")){
+            props.obj.school=props.column.split("_")[0];
         }
         e.preventDefault();
-        // console.log(obj);
+        // console.log(props.obj);
         //route to a post to save the data
-        if(action==="edit"){
+        if(props.action==="edit"){
             axios
-                .put(`/api/${col}/${obj.id}/`,obj)
+                .put(`/api/${col}/${props.obj.id}/`,props.obj)
                 .then(res =>{
-                    // console.log(obj)
-                    navigate(`/admin/${col}/${obj.id}/`)
+                    // console.log(props.obj)
+                    navigate(`/admin/${col}/${props.obj.id}/`)
 
                 }).catch(err => console.log(err));
-        }else if(action==="new"){
+        }else if(props.action==="new"){
             console.log("new")
-            console.log(obj)
+            console.log(props.obj)
             axios
-                .post(`/api/${col}/`,obj)
+                .post(`/api/${col}/`,props.obj)
                 .then(res =>{
                     navigate(`/admin/${col}s/`)
 
@@ -46,19 +48,19 @@ function EditForm({column, fields, obj, setobj, action}) {
         <div>
             <form>
                 <div className="form-inner">
-                    <h2>{action+" "+column}</h2>
+                    <h2>{props.action+" "+props.column}</h2>
                     {
-                        fields.filter(f=>f!=="id"&&f!=="admin").map((field,i)=>{
+                        props.fields.filter(f=>f!=="id"&&f!=="admin").map((field,i)=>{
                             return (<div className="form-group" key={i}>
                                 <label htmlFor={field}>{field}</label>
                                 <input
                                     className="input"
-                                    type={typeof(obj[field])}
+                                    type={typeof(props.obj[field])}
                                     name={field}
                                     id={field}
-                                    value={obj[field]}
+                                    value={props.obj[field]}
                                     onChange={(e)=>{
-                                        setobj({...obj, [field]: e.target.value})
+                                        setprops.obj({...props.obj, [field]: e.target.value})
                                     }}
                                 />
                             </div>)
@@ -73,4 +75,16 @@ function EditForm({column, fields, obj, setobj, action}) {
     )
 }
 
-export default EditForm
+EditForm.propTypes = {
+    column: PropTypes.string,
+    fields: PropTypes.arrayOf(PropTypes.string),
+    setobj: PropTypes.func,
+    action: PropTypes.func
+}
+
+const mapStateToProps = (state) => ({
+
+});
+
+export default connect(mapStateToProps)(EditForm)
+
