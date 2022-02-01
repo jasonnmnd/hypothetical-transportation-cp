82e91a7d71d3c83e51fa71b1a659d590f4a1a697
George Hong <grh17@duke.edu>
2022-01-31 23:21:57 -0500

    Tighten model restrictions on text fields
    Required text must be at least one character long

--- hypothetical_transportation/backend/models.py
+++ hypothetical_transportation/backend/models.py
@@ -1,17 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.core.validators import MinLengthValidator
 # Create your models here.
 from django.conf import settings
 
 
 class School(models.Model):
-    name = models.CharField(max_length=150)
-    address = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, validators=[MinLengthValidator(1)])
+    address = models.CharField(max_length=150, validators=[MinLengthValidator(1)])
 
 
 class Route(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, validators=[MinLengthValidator(1)])
     description = models.TextField(blank=True)
     school = models.ForeignKey(
         School, related_name='routes',
@@ -22,7 +22,7 @@ class Route(models.Model):
 class Student(models.Model):
     # first_name = models.CharField(max_length=30)
     # last_name = models.CharField(max_length=30)
-    full_name = models.CharField(max_length=150)
+    full_name = models.CharField(max_length=150, validators=[MinLengthValidator(1)])
     active = models.BooleanField(default=True)
     school = models.ForeignKey(
         School, related_name='students',
@@ -30,10 +30,10 @@ class Student(models.Model):
     )
     routes = models.ForeignKey(
         Route, related_name='students',
-        on_delete=models.SET_NULL, null=True
+        on_delete=models.SET_NULL, null=True, blank=True
     )
     guardian = models.ForeignKey(
         settings.AUTH_USER_MODEL, related_name='students',
-        on_delete=models.CASCADE, null=True
+        on_delete=models.CASCADE, null=True, blank=True
     )
     student_id = models.PositiveIntegerField(null=True)
