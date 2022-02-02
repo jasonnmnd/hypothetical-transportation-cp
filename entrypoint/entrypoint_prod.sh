f6d3e79ce15c43408f4c58da9d30f0bd9bb63b13
Dana Mulligan <55402598+danamulligan@users.noreply.github.com>
2022-02-01 20:51:02 -0500

    Update entrypoint_prod.sh

--- entrypoint/entrypoint_prod.sh
+++ entrypoint/entrypoint_prod.sh
@@ -2,6 +2,6 @@
 
 python /code/manage.py makemigrations
 python /code/manage.py migrate
-python /code/manage.py loaddata hypothetical_transportation/backend/fixtures/data.json
+python /code/manage.py loaddata code/backend/fixtures/data.json
 python /code/manage.py initadmin
 python /code/manage.py runserver_plus 0.0.0.0:8000 --cert-file /etc/letsencrypt/live/hypothetical_transportation.colab.duke.edu/fullchain.pem --key-file /etc/letsencrypt/live/hypothetical_transportation.colab.duke.edu/privkey.pem
