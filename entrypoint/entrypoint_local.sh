292931e28d85dad247e4e31891b325d85be54ef9
Dana Mulligan <55402598+danamulligan@users.noreply.github.com>
2022-02-01 20:50:48 -0500

    Update entrypoint_local.sh

--- entrypoint/entrypoint_local.sh
+++ entrypoint/entrypoint_local.sh
@@ -2,6 +2,6 @@
 
 python /code/manage.py makemigrations
 python /code/manage.py migrate
-python /code/manage.py loaddata hypothetical_transportation/backend/fixtures/data.json
+python /code/manage.py loaddata code/backend/fixtures/data.json
 python /code/manage.py initadmin
 python /code/manage.py runserver 0.0.0.0:8000
