0e44078dc3eadb1d4195442d694e5a89496ae21d
VCM admin account <vcm@vcm-24896.vm.duke.edu>
2022-02-01 19:02:23 -0500

    entrypoint

---
+++ entrypoint_dev.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python /code/manage.py makemigrations
+python /code/manage.py migrate
+python /code/manage.py loaddata hypothetical_transportation/backend/fixtures/data.json
+python /code/manage.py initadmin
+python /code/manage.py runserver_plus 0.0.0.0:8000 --cert-file /etc/letsencrypt/live/ht-dev.colab.duke.edu/fullchain.pem --key-file /etc/letsencrypt/live/ht-dev.colab.duke.edu/privkey.pem
