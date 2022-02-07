1a8f241a7a8e6fc7f7934e3896503d5f023903fa
George Hong <grh17@duke.edu>
2022-02-07 15:16:28 -0500

    Add stops, fields to School
    Also working on a way to maintain insertion order in Stops/Routes due to
    the requirement of ordered stops within a route.

--- hypothetical_transportation/backend/models.py
+++ hypothetical_transportation/backend/models.py
@@ -3,11 +3,22 @@ from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
 # Create your models here.
 from django.conf import settings
+import datetime
 
 
 class School(models.Model):
-    name = models.CharField(max_length=150, validators=[MinLengthValidator(1)])
+    name = models.CharField(max_length=150, validators=[MinLengthValidator(1)], unique=True)
     address = models.CharField(max_length=150, validators=[MinLengthValidator(1)])
+    bus_arrival_time = models.TimeField(blank=False, default=datetime.time(9, 0, 0))
+    bus_departure_time = models.TimeField(blank=False, default=datetime.time(15, 0, 0))
+
+    class Meta:
+        ordering = ['id']
+
+
+class Stop(models.Model):
+    name = models.CharField(max_length=150)
+    location = models.CharField(max_length=450)
 
     class Meta:
         ordering = ['id']
@@ -20,9 +31,19 @@ class Route(models.Model):
         School, related_name='routes',
         on_delete=models.CASCADE
     )
+    stops = models.ManyToManyField(Stop, through="StopRoute")
 
     class Meta:
         ordering = ['id']
+
+
+class StopRoute(models.Model):
+    stop = models.ForeignKey(Stop, on_delete=models.CASCADE)
+    route = models.ForeignKey(Route, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
 
 
 class Student(models.Model):
