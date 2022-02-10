677a863f1fd1f16b03e8d4df3fb483f55d6458c3
Lucy Gu <wg74@duke.edu>
2022-02-09 23:31:58 -0500

    call it search option instead of sort

--- hypothetical_transportation/frontend/src/components/adminPage/components/views/GeneralAdminTableView.js
+++ hypothetical_transportation/frontend/src/components/adminPage/components/views/GeneralAdminTableView.js
@@ -43,7 +43,7 @@ function GeneralAdminTableView( props ) {
 
     return (
         <div className="d-flex justify-content-space-between flex-column" style={{gap: "10px"}}>
-            <Button onClick={toggleSort} variant="yellowToggle">Sort Options</Button>
+            <Button onClick={toggleSort} variant="yellowToggle">Search Options</Button>
             {showSort ? (props.search != null && props.search != undefined ? <SearchBar buttons={getFilterOptions(props.tableType)} sortBy={getSortOptions(props.tableType)} search={props.search}></SearchBar> : null) : <></>}
             <GeneralTable rows={props.values} columnNames={getColumns(props.tableType)} actionName={props.actionName?props.actionName:"View"} action={props.action? props.action:handleViewClick}/>
             {props.pagination != null && props.pagination != undefined ? <PaginationButtons nextDisable={!props.values || props.values.length < pageSize} prefix={props.pagination}/> : null}
