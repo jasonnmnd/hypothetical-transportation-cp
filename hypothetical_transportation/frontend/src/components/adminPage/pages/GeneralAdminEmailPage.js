b0c444f0e2f7a43363712755eba85d86cb2cd4e6
jasonnmnd <85291489+jasonnmnd@users.noreply.github.com>
2022-02-09 21:19:49 -0500

    added selection options for school/route when sending an email

--- hypothetical_transportation/frontend/src/components/adminPage/pages/GeneralAdminEmailPage.js
+++ hypothetical_transportation/frontend/src/components/adminPage/pages/GeneralAdminEmailPage.js
@@ -1,9 +1,15 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../header/Header';
 import "../NEWadminPage.css";
-import { Container, Form, Button } from 'react-bootstrap'; 
+import { Container, Form, Button, ButtonGroup, ToggleButton } from 'react-bootstrap'; 
+import { connect } from 'react-redux';
+import { getSchools } from '../../../actions/schools';
+import { getRoutesByID } from '../../../actions/routes';
+import PropTypes from 'prop-types';
 
-function GeneralAdminEmailPage() {
+
+
+function GeneralAdminEmailPage(props) {
 
     const [subject, setSubject] = useState("");
     const [body, setBody] = useState("");
@@ -12,11 +18,69 @@ function GeneralAdminEmailPage() {
         console.log("Submit button pressed");
     }
 
+    const emailTypes = [
+        {name: "School", value: 1},
+        {name: "Route", value: 2}
+    ]
+
+    const [emailSelection, setEmailSelection] = useState(1);
+
+    useEffect(() => {
+        props.getSchools();
+    }, []);
   return (
     <>
         <Header></Header>
         <Container className="container-main">
             <Form className="shadow-lg p-3 mb-5 bg-white rounded">
+                <Container className='d-flex justify-content-center'>
+                    <Form.Group className="mb-3" controlId="validationCustom01">
+                        <ButtonGroup>
+                        {emailTypes.map((radio, idx) => (
+                            <ToggleButton
+                                key={idx}
+                                id={`radio-${idx}`}
+                                type="radio"
+                                variant={'outline-success'}
+                                name="radio"
+                                value={radio.value}
+                                checked={emailSelection == radio.value}
+                                onChange={(e)=>{
+                                    setEmailSelection(e.target.value);
+                                }}
+                            >
+                                {radio.name}
+                            </ToggleButton>
+                        ))}
+                        </ButtonGroup>
+                    </Form.Group>
+                </Container>
+
+                <Container className='d-flex flex-row justify-content-center' style={{gap: "20px"}}>
+                {
+                    emailSelection == 1 ? 
+                    <Form.Select size="sm" style={{width: "300px"}}>
+                        <option value={"null"} >{"-----"}</option>
+                        {props.schoollist!==null && props.schoollist!==undefined && props.schoollist.length!==0?props.schoollist.map((u,i)=>{
+                            return <option value={u.id} key={i}>{u.name}</option>
+                        }):null}
+                    </Form.Select>
+                    : 
+                    <>
+                       <Form.Select size="sm" style={{width: "300px"}}>
+                            {props.schoollist!==null && props.schoollist!==undefined && props.schoollist.length!==0?props.schoollist.map((u,i)=>{
+                                return <option value={u.id} key={i}>{u.name}</option>
+                            }):null}
+                        </Form.Select>
+
+                        <Form.Select size="sm" style={{width: "300px"}}>
+                            <option>Routes</option>
+                        </Form.Select> 
+                    </>
+                    
+                }
+                </Container>
+
                 <Form.Group className="mb-3" controlId="validationCustom01">
                     <Form.Label as="h5">Subject Title</Form.Label>
                     <Form.Control 
@@ -54,4 +118,14 @@ function GeneralAdminEmailPage() {
   );
 }
 
-export default GeneralAdminEmailPage;
+GeneralAdminEmailPage.propTypes = {
+    getRoutesByID: PropTypes.func.isRequired,
+    getSchools: PropTypes.func.isRequired,
+}
+
+const mapStateToProps = (state) => ({
+    schoollist: state.schools.schools.results,
+    routes: state.routes.routes.results,
+})
+
+export default connect(mapStateToProps, {getSchools, getRoutesByID}) (GeneralAdminEmailPage);
