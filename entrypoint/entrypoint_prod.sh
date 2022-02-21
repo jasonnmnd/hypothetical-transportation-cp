d5b83cbd5e6749dd73501e32bc8e415c47538965
Dana Mulligan <dana.mulligan@duke.edu>
2022-02-21 18:38:18 -0500

    upload prod stuff

--- entrypoint/entrypoint_prod.sh
+++ entrypoint/entrypoint_prod.sh
@@ -3,4 +3,5 @@
 python /code/manage.py makemigrations 
 python /code/manage.py migrate 
 python /code/manage.py initadmin
-python /code/manage.py runserver_plus 0.0.0.0:8000 --cert-file /etc/letsencrypt/live/hypothetical-transportation.colab.duke.edu/fullchain.pem --key-file /etc/letsencrypt/live/hypothetical-transportation.colab.duke.edu/privkey.pem
+# python /code/manage.py runserver_plus 0.0.0.0:8000 --cert-file /etc/letsencrypt/live/hypothetical-transportation.colab.duke.edu/fullchain.pem --key-file /etc/letsencrypt/live/hypothetical-transportation.colab.duke.edu/privkey.pem
+gunicorn hypothetical_transportation.wsgi:application --bind 0.0.0.0:8000
