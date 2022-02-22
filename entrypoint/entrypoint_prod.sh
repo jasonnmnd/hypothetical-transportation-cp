15435fa0e03a7a4774c941d19f2486c86e4e2179
VCM admin account <vcm@vcm-24706.vm.duke.edu>
2022-02-21 20:06:45 -0500

    life sucks

--- entrypoint/entrypoint_prod.sh
+++ entrypoint/entrypoint_prod.sh
@@ -5,5 +5,4 @@ python /code/manage.py migrate
 python /code/manage.py initadmin
 # python /code/manage.py runserver_plus 0.0.0.0:8000 --cert-file /etc/letsencrypt/live/hypothetical-transportation.colab.duke.edu/fullchain.pem --key-file /etc/letsencrypt/live/hypothetical-transportation.colab.duke.edu/privkey.pem
 cd code
-
-gunicorn a.wsgi:application --bind 0.0.0.0:8000
+gunicorn hypothetical_transportation.wsgi:application --bind 0.0.0.0:8000
