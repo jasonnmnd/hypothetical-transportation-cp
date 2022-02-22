7d78dedb0a6ccccbcb882580c0f1aa6cef1437ce
VCM admin account <vcm@vcm-24706.vm.duke.edu>
2022-02-21 21:12:12 -0500

    ahahahahahahah

--- entrypoint/entrypoint_prod.sh
+++ entrypoint/entrypoint_prod.sh
@@ -5,4 +5,6 @@ python /code/manage.py migrate
 python /code/manage.py initadmin
 # python /code/manage.py runserver_plus 0.0.0.0:8000 --cert-file /etc/letsencrypt/live/hypothetical-transportation.colab.duke.edu/fullchain.pem --key-file /etc/letsencrypt/live/hypothetical-transportation.colab.duke.edu/privkey.pem
 cd code
+export AUTHEMAIL_EMAIL_HOST_USER=hypotheticaltransportations@gmail.com
+export AUTHEMAIL_EMAIL_HOST_PASSWORD=avxkwyydulnzhudhgit 
 gunicorn hypothetical_transportation.wsgi:application --bind 0.0.0.0:8000
