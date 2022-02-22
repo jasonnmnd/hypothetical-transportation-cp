1350d230e3bd1b03cd6e0a109717791785b860a9
VCM admin account <vcm@vcm-24706.vm.duke.edu>
2022-02-21 19:30:48 -0500

    im just stupid

--- entrypoint/entrypoint_prod.sh
+++ entrypoint/entrypoint_prod.sh
@@ -4,4 +4,5 @@ python /code/manage.py makemigrations
 python /code/manage.py migrate 
 python /code/manage.py initadmin
 # python /code/manage.py runserver_plus 0.0.0.0:8000 --cert-file /etc/letsencrypt/live/hypothetical-transportation.colab.duke.edu/fullchain.pem --key-file /etc/letsencrypt/live/hypothetical-transportation.colab.duke.edu/privkey.pem
+cd code
 gunicorn hypothetical_transportation.wsgi:application --bind 0.0.0.0:8000
