bebace5fc642b49a165b7c80744e64bb205e0b0b
George Hong <grh17@duke.edu>
2022-02-23 21:04:14 -0500

    Remove auto seed due to cost on API

--- entrypoint/entrypoint_prod.sh
+++ entrypoint/entrypoint_prod.sh
@@ -5,7 +5,6 @@ python /code/manage.py migrate
 python /code/manage.py initadmin
 # python /code/manage.py runserver_plus 0.0.0.0:8000 --cert-file /etc/letsencrypt/live/hypothetical-transportation.colab.duke.edu/fullchain.pem --key-file /etc/letsencrypt/live/hypothetical-transportation.colab.duke.edu/privkey.pem
 cd code
-python manage.py seeddb_v2
 export AUTHEMAIL_EMAIL_HOST_USER=hypotheticaltransportations@gmail.com
 export AUTHEMAIL_EMAIL_HOST_PASSWORD=avxkwyydulnzhudh
 gunicorn hypothetical_transportation.wsgi:application --bind 0.0.0.0:8000
