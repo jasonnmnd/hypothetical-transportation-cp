96ea9d73236f5d64104680a3aa22a195f23b82b7
Lucy Gu <wg74@duke.edu>
2022-02-23 22:14:30 -0500

    console log removed

--- hypothetical_transportation/frontend/src/components/maps/MapComponent.js
+++ hypothetical_transportation/frontend/src/components/maps/MapComponent.js
@@ -179,8 +179,8 @@ function MapComponent(props) {
     const [set,setSet] = useState(false)
     const [curZoom, setCurZoom]= useState(0)
     useEffect(()=>{
-        console.log(props.center)
-        console.log(set)
+        // console.log(props.center)
+        // console.log(set)
         if(!isNaN(props.center.lat) && !isNaN(props.center.lng) && pos.lng!==props.center.lng && !set){
             setPos(props.center)
             // setSet(true)
