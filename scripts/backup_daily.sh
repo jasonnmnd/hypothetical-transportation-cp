d814be2a90fe046b7cae9b46feff2d35a75cb5e6
VCM admin account <vcm@ht-dev.colab.duke.edu>
2022-03-14 10:22:47 -0400

    backup scripts

---
+++ scripts/backup_daily.sh
@@ -0,0 +1,23 @@
+# setup, no password
+# ssh-keygen -t rsa 
+# ssh-copy-id vcm@vcm-25708.vm.duke.edu
+DOW=$(date +%a)
+echo "[BACKUP - DAILY] today is $DOW, starting daily backup"
+
+echo "[BACKUP - DAILY] dumping data from inside web app"
+sudo docker exec -it ece-458_web_1 python3 /code/manage.py dumpdata -o daily.json
+
+echo "[BACKUP - DAILY] copying data dump from db to server host"
+sudo docker cp ece-458_web_1:daily.json /home/vcm/daily-$DOW.json
+
+SSH_CONNECTION=$(ip route get 8.8.8.8 | grep -oP 'src \K[^ ]+')
+echo "i am $SSH_CONNECTION"
+
+cat /home/vcm/.ssh/id_rsa.pub
+cat /home/vcm/.ssh/known_hosts
+echo ""
+echo ""
+env
+
+echo "[BACKUP - DAILY] copying data dump from server host to backup host"
+scp -v -i /home/vcm/.ssh/id_rsa /home/vcm/daily-$DOW.json vcm@vcm-25708.vm.duke.edu:/home/vcm/backups/dev-host/daily/daily-$DOW.json
---
+++ scripts/backup_send_monthly.sh
@@ -0,0 +1,4 @@
+MONTH=$(date +%m)
+echo "Sending $MONTH monthly backup"
+
+scp -v -i ~/.ssh/id_rsa monthly-$MONTH.json vcm@vcm-25708.vm.duke.edu:backups/dev-host/monthly/monthly-$MONTH.json
