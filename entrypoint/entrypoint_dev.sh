5fdb2bd4814608d38cd46232158e0cd20450daed
VCM admin account <vcm@ht-dev.colab.duke.edu>
2022-03-21 18:42:01 -0400

    go again

--- entrypoint/entrypoint_dev.sh
+++ entrypoint/entrypoint_dev.sh
@@ -3,7 +3,8 @@
 python /code/manage.py makemigrations 
 python /code/manage.py migrate 
 python /code/manage.py initadmin
-python /code/manage.py loaddata backend/fixtures/schools.json backend/fixtures/groups.json
+# python /code/manage.py loaddata backend/fixtures/schools.json backend/fixtures/groups.json
 # python /code/manage.py runserver_plus 0.0.0.0:8000 --cert-file /etc/letsencrypt/live/ht-dev.colab.duke.edu/fullchain.pem --key-file /etc/letsencrypt/live/ht-dev.colab.duke.edu/privkey.pem
 cd code
+python manage.py loaddata backend/fixtures/schools.json backend/fixtures/groups.json
 gunicorn hypothetical_transportation.wsgi:application --bind 0.0.0.0:8000
