71c15d708e1b372b02ead49f3a5ed7c747d829bb
Milan Bhat <milan@Milans-MacBook-Air.local>
2022-03-21 20:39:06 -0400

    whe

--- entrypoint/entrypoint_dev.sh
+++ entrypoint/entrypoint_dev.sh
@@ -5,4 +5,4 @@ python /code/manage.py migrate
 python /code/manage.py initadmin
 # python /code/manage.py runserver_plus 0.0.0.0:8000 --cert-file /etc/letsencrypt/live/ht-dev.colab.duke.edu/fullchain.pem --key-file /etc/letsencrypt/live/ht-dev.colab.duke.edu/privkey.pem
 cd code
-gunicorn hypothetical_transportation.wsgi:application --bind 0.0.0.0:8000 --timeout 300
+gunicorn hypothetical_transportation.wsgi:application --bind 0.0.0.0:8000 --timeout 300 --threads 2
