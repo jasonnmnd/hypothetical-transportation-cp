b4cbc52d7bae24deb6be32d4cceeec3b1f2ac718
George Hong <grh17@duke.edu>
2022-03-31 15:43:12 -0400

    Add student account linking

--- hypothetical_transportation/backend/validators.py
+++ hypothetical_transportation/backend/validators.py
@@ -1,7 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
-from .models import Student
 
 
 def validate_available_user_email(value):
