339122bfb796246fad8df4e234d89b396d5371e6
Dana Mulligan <dana.mulligan@duke.edu>
2022-04-03 14:47:36 -0400

    delete unused endpoints

--- hypothetical_transportation/backend/urls.py
+++ hypothetical_transportation/backend/urls.py
@@ -1,8 +1,8 @@
 from rest_framework import routers
 from django.urls import path
 
-from .api import ActiveBusRunViewSet, BusRunViewSet, UserViewSet, StudentViewSet, RouteViewSet, SchoolViewSet, StopPlannerAPI, StopViewSet, \
-    VerifyLoadedDataAPI, SubmitLoadedDataAPI, TransitLogViewSet, StartBusRunAPI
+from .api import BusRunViewSet, UserViewSet, StudentViewSet, RouteViewSet, SchoolViewSet, StopPlannerAPI, StopViewSet, \
+    VerifyLoadedDataAPI, SubmitLoadedDataAPI, StartBusRunAPI, ActiveBusRunViewSet
 from rest_framework.urlpatterns import format_suffix_patterns
 
 router = routers.DefaultRouter()
@@ -11,8 +11,6 @@ router.register('route', RouteViewSet, 'route')
 router.register('stop', StopViewSet, 'stop')
 router.register('school', SchoolViewSet, 'school')
 router.register('student', StudentViewSet, 'student')
-router.register('bus', ActiveBusRunViewSet, 'bus')
-router.register('transit_log', TransitLogViewSet, 'transit_log')
 router.register('run', BusRunViewSet, 'run')
 
 additional_patterns = [
