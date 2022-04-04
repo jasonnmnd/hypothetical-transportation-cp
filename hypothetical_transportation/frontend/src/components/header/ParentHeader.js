beeec59b4859dcb3896298929bb84856cdb7676c
Lucy Gu <wg74@duke.edu>
2022-04-04 15:57:00 -0400

    header style

--- hypothetical_transportation/frontend/src/components/header/ParentHeader.js
+++ hypothetical_transportation/frontend/src/components/header/ParentHeader.js
@@ -18,10 +18,11 @@ function ParentHeader(props) {
                 </Nav.Link>
             <Navbar.Toggle/>
             
-            <Navbar.Collapse>
-                <Nav>
-                    {/* <Navbar.Brand style={{width:'1100px'}}>
-                    </Navbar.Brand> */}
+            <Navbar.Collapse style={{height:"40px"}}>
+                <Nav >
+                    {/* <NavDropdown title="">
+                    </NavDropdown> */}
+                    <div style={{position:'relative',height:'40px'}}></div>
                     <Nav.Link as={Link} to={`/account`} style={{position:'absolute',right:'90px'}}> Account</Nav.Link>
                     <Nav.Link onClick={props.logout} style={{position:'absolute',right:'20px'}}> Logout</Nav.Link>
                 </Nav>
