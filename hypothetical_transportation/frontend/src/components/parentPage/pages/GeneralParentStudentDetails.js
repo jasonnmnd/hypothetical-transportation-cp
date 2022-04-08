56d3c8644ed5520e28eacbb4aaaa6913a532c245
Milan Bhat <milan@Milans-MacBook-Air.local>
2022-04-08 00:22:18 -0400

    made a map component

--- hypothetical_transportation/frontend/src/components/parentPage/pages/GeneralParentStudentDetails.js
+++ hypothetical_transportation/frontend/src/components/parentPage/pages/GeneralParentStudentDetails.js
@@ -20,8 +20,6 @@ import { getRunByRoute } from "../../../actions/drive";
 function ParentStudentDetails(props){
     const param = useParams();
     const student = props.student;
-    const [pinData, setPinData] = useState([]);
-    const [extraComponents, setExtraComponents] = useState(null);
     let [searchParams, setSearchParams] = useSearchParams();
 
 
@@ -34,9 +32,6 @@ function ParentStudentDetails(props){
 
     }
 
-    useEffect(()=>{
-        setPinData(getPinData());
-    },[props.stops,student, props.activeRun])
 
 
     useEffect(()=>{
@@ -58,116 +53,9 @@ function ParentStudentDetails(props){
 
 
 
-    const getPinData = () => {
-        let pinData = getStopPinData();
-        addStudentPin(pinData, onStudentClick)
-        addBusPin(pinData)
-        // console.log(pinData);
-        return pinData;
-    }
-    const getStudentPin = (s) => {
-        return {
-            ...s, 
-            address: s.guardian.address, 
-            latitude: s.guardian.latitude, 
-            longitude: s.guardian.longitude
-        }
-    }
-    const getStopPin = (stop) => {
-        return {
-            ...stop, 
-        }
-    }
-
-    const addStudentPin = (pinData, onclick) => {
-        pinData.push({
-            iconColor: "green",
-            iconType: "studentCheck",
-            markerProps: {
-                onClick: onclick
-            },
-            pins: [
-                getStudentPin(student)
-            ]
-        })
-    }
-
-    const getRunPin = () => {
-        console.log(props)
-        return {
-            ...props.activeRun, 
-            latitude: props.activeRun.location.latitude, 
-            longitude: props.activeRun.location.longitude, 
-        }
-    }
-
-    const addBusPin = (pinData) => {
-        if(props.activeRun.driver == null){
-            return
-        }
-        pinData.push({
-            iconColor: "black",
-            iconType: "bus",
-            markerProps: {
-                onClick: onBusClick,
-            },
-            pins: [
-                getRunPin()
-            ]
-        })
-    }
-
-    const getBusInfoForWindow = (pinStuff) => {
-        return (
-            <>
-                <h4>Bus {pinStuff.bus_number}</h4>
-                <h5>Driver: {pinStuff.driver.full_name}</h5>
-                <h5>Route: {pinStuff.route.name}</h5>
-            </>
-            
-        )
-    }
-
-    const onBusClick = (pinStuff, position) => {
-        createInfoWindow(position, getBusInfoForWindow(pinStuff))
-    }
-    
-
-    const createInfoWindow = (position, windowComponents) => {
-        setExtraComponents(<InfoWindow position={position} onCloseClick={setExtraComponents(null)}>{windowComponents}</InfoWindow>)
-    }
 
 
-    const getStopPinData = () => {
-        return [
-            {
-                iconColor: "blue",
-                iconType: "stop",
-                markerProps: {
-                    onClick: onStopClick,
-                    draggable: false,
-                    onRightClick: ""
-                },
-                pins: props.stops.map(stop => getStopPin(stop))
-            },
-        ]
-    }
 
-    const onStopClick = (pinStuff, position) => {
-        createInfoWindow(position, 
-            <div>
-                <h5>Name:{pinStuff.name}</h5>
-                <h5>Pick Up: {pinStuff.pickup_time}</h5>
-                <h5>Drop Off: {pinStuff.dropoff_time}</h5>
-            </div>
-        )
-    }
-
-    const onStudentClick = (pinStuff, position) => {
-        createInfoWindow(position, 
-            <><h4>{pinStuff.full_name}</h4></>
-        )
-    }
 
 
     return(
@@ -290,7 +178,8 @@ function ParentStudentDetails(props){
             <Container className='d-flex flex-column justify-content-center' style={{marginTop: "20px"}}>
                 <IconLegend legendType='parentStudent'></IconLegend>
                 <Card.Body style={{padding: "0px",marginTop: "20px",marginBottom: "20px"}}>
-                    <MapComponent pinData={pinData} otherMapComponents={extraComponents} center={{lng: Number(props.student.guardian.longitude),lat: Number(props.student.guardian.latitude)}}></MapComponent>
+                    {/* <MapComponent pinData={pinData} otherMapComponents={extraComponents} center={{lng: Number(props.student.guardian.longitude),lat: Number(props.student.guardian.latitude)}}></MapComponent> */}
+                    <StudentViewMap student={props.student} activeRun={props.activeRun} stops={props.stops} />
                 </Card.Body>    
             </Container>
             :
