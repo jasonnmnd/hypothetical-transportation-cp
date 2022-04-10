ba9fd69ad007d60f116c5a4dca535b09e93c27bb
Dana Mulligan <dana.mulligan@duke.edu>
2022-04-10 15:31:27 -0400

    mergingggg

--- hypothetical_transportation/hypothetical_transportation/settings.py
+++ hypothetical_transportation/hypothetical_transportation/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import sys
-import logging
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +26,7 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECRET_KEY = 'django-insecure-dz)xt+ggvmpya26p(yn$y-0gcq1&$tnrj+i_n5*1u0_ek+j3lg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 LOGGING = {
     'version': 1,
@@ -69,7 +67,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.postgres',
     'django.contrib.staticfiles',
-    'django_crontab',
     'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
@@ -199,10 +196,6 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
 
-CRONJOBS = [
-    ('*/10 * * * * *', 'backend.cron.tranzit_traq_on_active_buses')
-]
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_FROM = os.environ.get('AUTHEMAIL_DEFAULT_EMAIL_FROM') or 'NOREPLY@hypotheticaltransportation.com'
@@ -215,3 +208,14 @@ EMAIL_HOST_PASSWORD = os.environ.get('AUTHEMAIL_EMAIL_HOST_PASSWORD') or ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
+# CELERY_BROKER_URL = 'amqp://guest@broker:5672'
+# CELERY_ACCEPT_CONTENT = ['application/json']
+# CELERY_RESULT_SERIALIZER = 'json'
+# CELERY_TASK_SERIALIZER = 'json'
+# # CELERY_IMPORTS = [
+# #     'backend.tasks2'
+# # ]
+
+# CELERY_RESULT_BACKEND = 'django-db'
+# CELERY_CACHE_BACKEND = 'django-cache'
+# # CELERY_TIMEZONE = 'Europe/Amsterdam'
